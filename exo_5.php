a5c0df30c3f7b3df616e1204e9d453b6a8c26b0d
julie-devos <julie.devos@epitech.eu>
2020-12-07 15:07:37 +0100

    commit

---
+++ exo_5.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>PhpPart1Exo4</title>
+</head>
+<body>
+	<?php $age = NULL; ?>
+<p><?= gettype($age)?></p>
+	<?php $age = 5; ?>
+<p><?=$age?></p>
+</body>
+</html>
